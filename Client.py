c60933f52e7886e5361f7794e2f040914f208a7b
Brinza Bezrukoff <bezrukoff888@gmail.com>
2018-12-10 23:14:40 +0300

    Main.py transformed into Client.py

--- Client.py
+++ Client.py
@@ -1,13 +1,61 @@
-import socket
-import pickle
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from UI import Ui_MainWindow
+
+'''
+Объекты :
+pushMessage - Кнопка отправки введенного в поле Message
+Message - Поле для набора сообщения
+setNickname - Кнопка смены никнейма на тот, что указан в поле Nickname
+Nickname - Поле для набора никнейма 
+chat - Чат
+Port - Поле для набора порта
+PortCreation - Кнопка для создания сервера с указанным в поле Port портом
+PortConnection - Кнопка для подключения к указанному в поле Port порту
+
+Массивы :
+Добавил массив сообщений и его наполнение
+'''
 
 
-obj = ["Nickname", "Message"]
-output = pickle.dumps(obj)
-print(obj, output)
+class MyWidget(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.nick = "Неопознанный арбуз"
+        self.MAXLEN = 55
+        self.main()
+        self.messages = []
 
-sock = socket.socket()
-sock.connect(('localhost', 9090))
-sock.send(output)
+    def main(self):
+        self.pushMessage.clicked.connect(self.writeMessage)
+        self.setNickname.clicked.connect(self.changeNickname)
 
-sock.close()
+    def writeMessage(self):
+        startmessage = len(self.nick) + 2
+        temp = self.Message.text()
+        self.chat.addItem(self.nick + ": " + self.Message.text()[: self.MAXLEN - startmessage])
+        for i in range(1, len(temp) // self.MAXLEN + 1):
+            self.chat.addItem(temp[startmessage + (i - 1) * self.MAXLEN: i * self.MAXLEN])
+        self.Message.setText("")
+        self.messages.append(temp)
+
+    def changeNickname(self):
+        temp = self.Nickname.text().strip()
+        if temp == "":
+            print("Нельзя сменить имя")
+        elif len(temp) > 20:
+            print("Слишком длинное имя")
+        else:
+            oldNick = self.nick
+            self.nick = temp
+            self.chat.addItem("{} сменил имя на {}".format(oldNick, self.nick))
+            self.showNick.setText(self.nick)
+        self.Nickname.setText("")
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
