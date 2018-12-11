7d623533e0f7b15edbce7e2130d56fa50233622f
Stasivs <stasivs@bk.ru>
2018-12-12 02:15:37 +0300

    Добавил папку Commands для exe'шников

---
+++ Release/dist/Commands/Setnick.py
@@ -0,0 +1,19 @@
+from Command import *
+
+
+class Setnick (Command):
+    def load(self):
+        self.name = "setnick"
+        self.syntax = "<nick:text>"
+        self.help = "Change your nick"
+        self.permissions = Permissions.User
+
+    def execute(self, args, caller):
+        nick = self.getStr(args, 0)
+        try:
+            self.get_user_by_nick(nick)
+        except CmdError:
+            caller.nick = nick
+            raise CmdAnswer("Nick successful changed to {}".format(nick))
+        else:
+            raise CmdError("Nick {} already in use".format(nick))
